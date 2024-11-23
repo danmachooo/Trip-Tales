4fca7d69d06654a00ee0737e2cab83474cd1ad07
danmachooo <johnpauldanmachi@gmail.com>
2024-11-23 21:32:36 +0800

    hello

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -122,11 +122,17 @@ include APP_DIR.'views/templates/header.php';
                                 endif;
                             ?>
 
-                            <div class="bg-gray-100 rounded-lg h-64 flex items-center justify-center mb-4">
-                                <svg class="w-12 h-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
-                                </svg>
-                            </div>
+                             <!-- Photo of the Post -->
+                             <?php if (!empty($post['photo_url'])): ?>
+                                <div class="mb-4">
+                                    <img 
+                                        src="\public\<?= htmlspecialchars($post['photo_url']) ?>" 
+                                        alt="Post Image" 
+                                        class="w-full h-64 object-cover rounded-lg"
+                                    />
+                                </div>
+                            <?php endif; ?>
+
                             <!-- New Map Area -->
                             <div class="mb-4">
                                 <h4 class="font-bold mb-2">Location</h4>
